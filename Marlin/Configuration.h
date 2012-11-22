bdf50531417bf986695305c64ef4ddedc68d9e0f
Alessandro Ranellucci <aar@cpan.org>
2012-11-22 14:31:55 +0100

    Set 115200

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -12,8 +12,8 @@
 #define STRING_CONFIG_H_AUTHOR "Al" //Who made the changes.
 
 // This determines the communication speed of the printer
-#define BAUDRATE 250000
-//#define BAUDRATE 115200
+//#define BAUDRATE 250000
+#define BAUDRATE 115200
 
 //// The following define selects which electronics board you have. Please choose the one that matches your setup
 // Gen7 custom (Alfons3 Version) = 10 "https://github.com/Alfons3/Generation_7_Electronics"
