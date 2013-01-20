2a4d5cb728c8a9bf760f9656f24f3ff5fd1b341f
Alessandro Ranellucci <aar@cpan.org>
2013-01-20 19:15:19 +0100

    Raise retract accel

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -274,16 +274,16 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 // default settings 
 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,2560,700}  // default steps per unit for ultimaker 
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 3, 45}    // (mm/sec)    
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 3, 50}    // (mm/sec)    
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,50,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          1500    // X, Y, Z and E max acceleration in mm/s^2 for printing moves 
-#define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for r retracts
+#define DEFAULT_RETRACT_ACCELERATION  4000   // X, Y, Z and E max acceleration in mm/s^2 for r retracts
 
 // 
 #define DEFAULT_XYJERK                16.0    // (mm/sec)
 #define DEFAULT_ZJERK                 0.4     // (mm/sec)
-#define DEFAULT_EJERK                 2.0    // (mm/sec)
+#define DEFAULT_EJERK                 5.0    // (mm/sec)
 
 //===========================================================================
 //=============================Additional Features===========================
